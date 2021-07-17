22e1034468787389d4f3587b15f039d784614bb8
josue carlos <josuecarlosos2@gmail.com>
2021-07-17 03:59:25 -0300

    proj

---
+++ AlgoritmoUFRN/x.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+
+using std::stringstream;
+stringstream buffer;
+using namespace std;
+
+
+int main() {
+
+	count << "pça";
+}
